9f7392a823a48e6a37a63c73649c2f1660393198
Yuki Sawa <yukisawa@gmail.com>
2020-10-15 19:38:37 -0700

    init backend
    Signed-off-by: Yuki Sawa <yukisawa@gmail.com>

---
+++ backend/apig.py
@@ -0,0 +1,79 @@
+import json
+from typing import Union
+
+import pendulum
+
+
+def as_apigateway_event(body):
+    return {"body": json.dumps(body)}
+
+
+class APIGatewayEvent:
+    def __init__(self, event: dict):
+        self.event = event
+        self.headers = event.get("headers", {})
+
+        path = event.get("path", "")  # ex: "/qa"
+        if path.endswith("/"):
+            path = path[:-1]
+        self.path = path
+
+        if event["body"]:
+            try:
+                payload = json.loads(event["body"])
+            except json.JSONDecodeError as e:
+                print(type(e), e)
+                raise ValueError(
+                    f"JSON validation failed. Invalid JSON most likely, {type(e)}, {e}"
+                )
+        else:
+            payload = {}
+
+        self.payload = payload
+        self.debug = payload.get("debug", False)
+
+        self.resp_ok = {"status": "ok"}
+        self.status_200 = 200
+        self.status_400 = 400
+
+        self.time_now = pendulum.now("America/Los_Angeles")
+        self.time_now_str = self.time_now.to_datetime_string() + " PST"
+
+    def __str__(self):
+        s = f"APIGatewayEvent: {self.headers}, {self.path}, {self.payload}"
+        return s
+
+
+def init_api_event(event: dict) -> APIGatewayEvent:
+    """ Take API Gateway event from lambda_handler and parse it"""
+    return APIGatewayEvent(event)
+
+
+def as_api_gateway_response(
+    body: Union[dict, list, str], status: int, resp_type: str = "json"
+) -> dict:
+    headers = {
+        "Access-Control-Allow-Origin": "*",  # Required for CORS support to work
+        "Access-Control-Allow-Credentials": True,  # Required for cookies, authorization headers with HTTPS
+    }
+
+    if resp_type == "json":
+        if isinstance(body, dict):
+            body = json.dumps(body, default=str)
+        else:
+            body = json.dumps({"status": body}, default=str)
+    elif resp_type == "text":
+        headers["Content-Type"] = "text/plain"
+    else:
+        pass
+
+    return {
+        "statusCode": status,
+        "headers": headers,
+        "body": body,
+        "isBase64Encoded": False,
+    }
+
+
+if __name__ == "__main__":
+    pass
