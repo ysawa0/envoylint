8f4599f0c02b65e638497f5a677d63bd13c4c168
Yuki Sawa <yukisawa@gmail.com>
2020-10-15 19:13:58 -0700

    init frontend
    Signed-off-by: Yuki Sawa <yukisawa@gmail.com>

---
+++ frontend/src/App.jsx
@@ -0,0 +1,130 @@
+import React from "react"
+import {Row, Col, Divider,} from "antd"
+import "./App.less"
+import "./main.css"
+import Linter from "./linter"
+
+import {Image, Card, Layout, Menu} from 'antd'
+import {HomeOutlined} from '@ant-design/icons'
+import {Tweet} from 'react-twitter-widgets'
+
+const {Header, Content, Footer} = Layout
+
+
+const logo = require('./img/envoy-horizontal-color.svg')
+
+
+const App = () => (
+    <Layout className="layout" style={{backgroundColor: "#1a1b1e"}}>
+        <Header>
+            <div className="logo"/>
+            <Menu mode="horizontal" defaultSelectedKeys={["1"]}>
+                <Menu.Item key="1" icon={<HomeOutlined/>}>Envoy Config Linter and Validator Online</Menu.Item>
+            </Menu>
+        </Header>
+        <Content style={{padding: "0 50px"}}>
+            <Linter/>
+            <Divider orientation="left">About</Divider>
+            <Row gutter={{xs: 8, sm: 16, md: 24, lg: 32}} className="centerh">
+                <Col style={{span: 12}}>
+                    <div>
+                        <Card
+                            title="What is this?"
+                            className="aboutcards"
+                            bordered={true}
+                            style={{marginBottom: 16}}
+                            headStyle={{fontSize: "20px"}}
+                        >
+                            <p>
+                                This site takes a{" "}
+                                <a href={"https://www.envoyproxy.io/"}>Envoy</a> config and
+                                validates it for you.
+                            </p>
+                            Inspired by:
+                            <Tweet tweetId="1306060212244111360"/>
+                        </Card>
+                        <Card
+                            title="How does it work?"
+                            className="aboutcards"
+                            bordered={true}
+                            style={{marginBottom: 16}}
+                            headStyle={{fontSize: "20px"}}
+                        >
+                            <p>
+                                It sends the config to a Lambda running the{" "}
+                                <a
+                                    href={
+                                        "https://www.envoyproxy.io/docs/envoy/latest/install/tools/config_load_check_tool"
+                                    }
+                                >
+                                    config_load_check_tool
+                                </a>{" "} in a Lambda layer
+                                and prints the results.
+                                <br/>
+                                There is a 30 second timeout on the linter due to API Gateway limitations. So currently,
+                                very very large configs will reach that.
+                            </p>
+                        </Card>
+                        <Card
+                            title="Why run it on AWS Lambda?"
+                            className="aboutcards"
+                            bordered={true}
+                            style={{marginBottom: 16}}
+                            headStyle={{fontSize: "20px"}}
+                        >
+                            <p>
+                                For fun and also for that sweet free 1 million requests a month.
+                            </p>
+                        </Card>
+                        <Card
+                            title="Do you save any data?"
+                            className="aboutcards"
+                            bordered={true}
+                            style={{marginBottom: 16}}
+                            headStyle={{fontSize: "20px"}}
+                        >
+                            <p>
+                                No and all sessions run on ephemeral Lambda containers. But it's
+                                best to never send any sensitive data.
+                            </p>
+                        </Card>
+                        <Card
+                            title="Can I see the code?"
+                            className="aboutcards"
+                            bordered={true}
+                            style={{marginBottom: 16}}
+                            headStyle={{fontSize: "20px"}}
+                        >
+                            <p>
+                                It will be open sourced once I have some time to clean up the code.
+                            </p>
+                        </Card>
+                        <Card
+                            title="Have any suggestions, questions, see any bugs?"
+                            className="aboutcards"
+                            bordered={true}
+                            style={{marginBottom: 16}}
+                            headStyle={{fontSize: "20px"}}
+                        >
+                            <p>
+                                Shoot me a message{" "}
+                                <a href={"https://twitter.com/yukisww"}>@yukisww</a> or email
+                                yuki@yukisawa.com
+                            </p>
+                        </Card>
+                    </div>
+                </Col>
+            </Row>
+        </Content>
+        <div style={{textAlign: "center", backgroundColor: "#28292a", height: "20px"}}>
+        </div>
+        <Footer style={{textAlign: "center", backgroundColor: "#505050"}}>
+            <Image width={200} src={logo} preview={false} style={{backgroundColor: "white"}}/>
+        </Footer>
+        <Footer style={{textAlign: "center", backgroundColor: "#505050"}}>
+            {" "}
+            Made for <a href={"https://www.envoyproxy.io/"}>Envoy Proxy</a>
+        </Footer>
+    </Layout>
+);
+export default App
---
+++ frontend/src/linter.jsx
@@ -0,0 +1,164 @@
+import React, {Component} from "react"
+import {Button, Divider} from "antd"
+import axios from "axios"
+import {Form, Input, Row, Col} from "antd"
+
+import {FireTwoTone, LoadingOutlined, CloseCircleTwoTone, CheckCircleTwoTone} from '@ant-design/icons'
+import {mockConf} from './util'
+
+
+const {TextArea} = Input
+const layout = {
+    labelCol: {
+        span: 0,
+    },
+    wrapperCol: {
+        span: 24,
+    },
+}
+
+export default class Linter extends Component {
+    state = {
+        conf: mockConf,
+        out: '',
+        pass: false,
+        fail: false,
+        load: false,
+        error: false,
+    }
+    formRef = React.createRef()
+    handleSubmit = e => {
+        e.preventDefault()
+        this.setState({load: true})
+        this.setState({pass: false, fail: false, error: false})
+        const conf = this.state.conf
+        const data = {conf: conf}
+        const url = 'https://wco1jydmml.execute-api.us-east-1.amazonaws.com/dev/linter'
+        axios.post(url, data, {timeout: 40000})
+            .then(res => {
+                let out = res.data.out.split("\n")
+                let err = res.data.err.split("\n")
+                let txt = []
+                let badConf = false
+                for (let i = 0; i < err.length; i++) {
+                    const c = err[i]
+                    if (c.startsWith("TestRandomGenerator")) {
+                        continue
+                    } else if (c.includes("testing /tmp/")) {
+                        continue
+                    }
+                    if (c.includes("INVALID_ARGUMENT")) {
+                        badConf = true
+                    }
+                    txt.push(c)
+                }
+                txt.push("------------")
+                for (let i = 0; i < out.length; i++) {
+                    const c = out[i]
+                    if (c.startsWith("TestRandomGenerator")) {
+                        continue
+                    }
+                    if (c.includes("INVALID_ARGUMENT")) {
+                        badConf = true
+                    }
+                    txt.push(c)
+                }
+                this.setState({load: false})
+                if (badConf) {
+                    this.setState({fail: true})
+                    for (let i = 0; i < txt.length; i++) {
+                        if (txt[i].includes("No failures.")) {
+                            txt[i] = txt[i].replace("No failures.", "")
+                        }
+                    }
+                } else if (res.data.code === 0) {
+                    this.setState({pass: true})
+                } else {
+                    this.setState({fail: true})
+                }
+
+                this.setState({out: txt.join("\n")})
+            })
+            .catch(res => {
+                this.setState({out: res.code + "\n" + res.message})
+                this.setState({error: true, load: false})
+            })
+    }
+
+    handleChange = e => {
+        this.setState({conf: this.formRef.current.getFieldValue("envoy-conf")})
+    }
+
+    render() {
+        return (
+            <>
+                <Row gutter={{xs: 8, sm: 16, md: 24, lg: 32}}>
+                    <Col className="gutter-row" span={12}>
+                        <Divider orientation="left">envoy conf</Divider>
+                        <Form
+                            {...layout}
+                            name="nest-messages"
+                            ref={this.formRef}
+                        >
+                            <Form.Item
+                                name="envoy-conf"
+                            >
+                                <Input.TextArea
+                                    rows={30}
+                                    value={this.state.conf}
+                                    onChange={this.handleChange}
+                                    defaultValue={mockConf}
+                                />
+                            </Form.Item>
+                            <Form.Item wrapperCol={{...layout.wrapperCol, offset: 0}}>
+                                <Button
+                                    type="primary"
+                                    htmlType="submit"
+                                    onClick={this.handleSubmit}
+                                >
+                                    Validate
+                                </Button>
+                            </Form.Item>
+                        </Form>
+                    </Col>
+                    <Col className="gutter-row" span={12}>
+                        <Divider orientation="left">results</Divider>
+                        <TextArea rows={30} value={this.state.out}/>
+                    </Col>
+                </Row>
+                <Row gutter={{xs: 8, sm: 16, md: 24, lg: 32}} className="centerh">
+                    <Col>
+                        <div
+                            style={{
+                                fontSize: "120px",
+                            }}
+                        >
+                            <LoadingOutlined
+                                style={!this.state.load ? {display: "none"} : {}}
+                            />
+                            <div style={!this.state.pass ? {display: "none"} : {}}>
+                                <CheckCircleTwoTone
+                                    twoToneColor="#52c41a"
+                                />
+                                {" "}config passed!
+                            </div>
+                            <div style={!this.state.fail ? {display: "none"} : {}}>
+                                <CloseCircleTwoTone
+                                    twoToneColor="#eb2f96"
+                                />
+                                {" "}config failed validation
+                            </div>
+                            <div style={!this.state.error ? {display: "none"} : {}}>
+                                <FireTwoTone
+                                    twoToneColor="#eb2f96"
+                                />
+                                api error
+                            </div>
+                        </div>
+                    </Col>
+                </Row>
+            </>
+        );
+    }
+}
+
