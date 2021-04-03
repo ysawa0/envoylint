17d1037c3833c39a00d1d4e5458bf54a66afb166
Yuki Sawa <yukisawa@gmail.com>
2021-04-02 18:22:10 -0700

    Update

--- get_envoy_binary.sh
+++ get_envoy_binary.sh
@@ -1,3 +1,5 @@
+# Kill any other docker containers already running before executing this
+# Places envoy binary into backend/bin/envoy-$1/envoy
 docker run -d envoyproxy/envoy:v$1
 mkdir -p backend/bin/envoy-$1
 docker cp $(docker ps -q):/usr/local/bin/envoy backend/bin/envoy-$1/
