58e3492982142c73172e4f91dbff36bac65d4fce
Yuki Sawa <yukisawa@gmail.com>
2023-11-12 20:03:30 -0800

    clean

--- get_envoy_binary.sh
+++ get_envoy_binary.sh
@@ -4,5 +4,5 @@
 
 docker run -t -d --platform=linux/amd64 envoyproxy/envoy-contrib:v$1
 mkdir -p backend/bin/envoy-$1
-docker cp $(docker ps -q):/usr/local/bin/envoy backend/bin/envoy-$1/
-docker kill $(docker ps -q)
+docker cp "$(docker ps -q)":/usr/local/bin/envoy backend/bin/envoy-$1/
+docker kill "$(docker ps -q)"
